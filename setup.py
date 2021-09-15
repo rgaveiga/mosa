41c84c84d0fcef1231e7c8c71ab8dfc2c3fe14e0
rgaveiga <88116667+rgaveiga@users.noreply.github.com>
2021-09-15 10:41:13 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os.path
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__),fname)).read()
+
+setup(name='mosa',
+      version='0.1',
+      description="Multi-objective Simulated Annealing (MOSA) implementation in pure Python.",
+      long_description_content_type='text/markdown',
+      long_description=read('Readme.MD'),
+      author='Roberto Gomes de Aguiar Veiga',
+      url="https://github.com/rgaveiga/mosa",
+      packages=['mosa'])
+
+
